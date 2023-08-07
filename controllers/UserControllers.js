7595dabdd0ea9d6eb9adb667c4c27e239306f4ac
steven attara <sattara1985@gmail.com>
2023-08-07 17:58:42 -0500

    error

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,13 +1,20 @@
 // funciones que resulevan el problemA
-const users = require('../models/UserFakeData')
-
+//const users = require('../models/UserFakeData')
+const usersData = require('../models/UserFakeData')
+const users = users.module
 
 module.exports = {
     getUsers: (req, res) => {
         res.status(200).send(users);
         //res.json(users)
       },
-    getOneUser: (req, res) => {},
+    getOneUser: (req, res) => {
+      //res.json(req.params.id); //validando que si tome el id del usuario
+      //const usersData = users.mudole
+      const idUser=req.params.id
+      const user=users.find(element)
+      //console.log(usersData)
+    },
     createUser: (req, res) => {},
     putUser: (req, res) => {},
     updateUser: (req, res) => {},
