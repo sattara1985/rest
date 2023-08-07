93b01e2860c00667af9fd434d4115e9450b6602b
steven attara <sattara1985@gmail.com>
2023-08-06 21:37:56 -0500

    fist endpoint

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log ("hola");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World steven!');
+})
+
+app.listen(port, () => {
+  console.log(`Aplicacion de ejemplo en el puerto: ${port}`);
+})
