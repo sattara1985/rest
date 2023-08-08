0c829a78b9b92901767ad5338763db08ca2f714d
steven attara <sattara1985@gmail.com>
2023-08-07 22:03:59 -0500

    correccion error

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,7 +1,7 @@
 // funciones que resulevan el problemA
 //const users = require('../models/UserFakeData')
 const usersData = require('../models/UserFakeData')
-const users = users.module
+const users = usersData.module
 
 module.exports = {
     getUsers: (req, res) => {
@@ -11,8 +11,19 @@ module.exports = {
     getOneUser: (req, res) => {
       //res.json(req.params.id); //validando que si tome el id del usuario
       //const usersData = users.mudole
+      console.log(typeof req.params.id, req.params.id)
       const idUser=req.params.id
-      const user=users.find(element)
+    
+      console.log(idUser)
+      const user=users.find(element => element.id == idUser)
+      console.log(user)
+      
+      if(user){
+        res.status(200).send(user)
+      }else{
+        res.status(404).send({message:'el usuario no existe'})
+      }
+     
       //console.log(usersData)
     },
     createUser: (req, res) => {},
--- models/UserFakeData.js
+++ models/UserFakeData.js
@@ -19,4 +19,4 @@ const users =[
     },
 ]
 
-exports.mudole = users
+exports.module = users
